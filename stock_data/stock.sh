1fa7ee18f279ed4ac284ade41349382d648a5b63
InuJang <qnrn90@gmail.com>
2017-11-20 01:40:49 -0800

    add LG display stock

--- stock_data/stock.sh
+++ stock_data/stock.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 wget "http://www.google.com/finance/historical?q=KRX%3AKOSPI&output=csv" -O kospi.txt
-wget "http://www.google.com/finance/historical?q=KRX%3AKOSPI&output=csv" -O Samsung_Electronics.txt
-
+wget "http://www.google.com/finance/historical?q=KRX:005930&output=csv" -O Samsung_Electronics.txt
+wget "http://www.google.com/finance/historical?q=KRX:034220&output=csv" -O LG_Display.txt
