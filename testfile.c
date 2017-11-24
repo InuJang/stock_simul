f8442aadff392c60c7828540f732a42771ad71e5
jungjunkim <jungjun9150@naver.com>
2017-11-23 19:38:28 -0800

    ui for menu

--- testfile.c
+++ testfile.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
-int main()
-{
-printf("**********************************************************\n");
-printf("******This is Stock Simulation of Embeddedsw Project******\n");
-printf("**********************************************************\n");
+#include <string.h>
+#include <stdlib.h>
+int MyMoney=10000000;
+void DisplayMenu();
+int main(){
+   int select;
+      
+   
+   DisplayMenu();
+   return 0;
+}
 
-return 0;
+void DisplayMenu(){
+   printf("**********************************************************\n");
+   printf("******This is Stock Simulation of Embeddedsw Project******\n");
+   printf("**********************************************************\n");
+   printf("1. Comfirm the corporation\n");
+   printf("2. Check the price-earnings ratio of corporation\n");
+   printf("3. Invest!!! \n");\
+   printf("**********************************************************\n");
+   printf("**********************************************************\n");
+   printf("**********************************************************\n");
+
 }
