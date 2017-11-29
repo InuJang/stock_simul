92a67d5b10c3f68886de59252185c90a68e4d52c
Jang <qnrn90@gmail.com>
2017-11-29 03:10:37 -0800

    make stock data only data, close

--- stock_data/parsing_stock.c
+++ stock_data/parsing_stock.c
@@ -7,9 +7,13 @@
 void main()
 {
 	int fd,fd2;
+	int fd3,fd4;
 	struct stat s_buf;
 	int SZ_BUF = 1024;
-	
+	char textbuf[4096];
+	char *line;
+	char ch;
+	int cnt = 0;
 	fd = open("Samsung_Electronics.txt",O_RDONLY);
 	if(fd == -1)
 	{
@@ -29,7 +33,34 @@ void main()
 	{
 		write(fd2,buf,nb);
 	}
+	fd3 = fopen ("test.txt","r+");
+	fd4 = fopen ("Samsung","w");
+	line = fgets(textbuf,sizeof(textbuf),fd3);
+	printf("%s",line);
+	while((ch = fgetc(fd3))!=EOF)
+	{
+		if(ch == '\n')
+		{
+			cnt = 0;
+		}
+		if(ch == ',')
+		{
+			cnt++;
+		}
+		if(cnt == 0)
+		{
+			fprintf(fd4,"%c",ch);
+		}
+		if(cnt == 4)
+		{	
+			fprintf(fd4,"%c",ch);
+		}
+
+	}
+	
 	printf("cp sucessful \n");
 	close(fd);
 	close(fd2);
+	close(fd3);
+	close(fd4);
 }
