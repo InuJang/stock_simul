130217cd320748d3ccdac6db8fdab9d2480a2dfc
jungjunkim <jungjun9150@naver.com>
2017-11-29 04:29:01 -0800

    add the menu

--- ui.c
+++ ui.c
@@ -7,14 +7,18 @@ void DisplayMenu();
 void Step_1();
 void Step_2();
 void Step_3();
+void Invest(int money);
 
+int Mymoney=0;
+int Cornum=-1;
+int step=-1;
+char Corporation[15][2]={SAMSUNG,HYUNDAI};
 int main(){
 
    DisplayMenu();
-   int step=-1;
-   
+ 
    printf("\nPress the number\n");
-   
+
    while(1)
    {
      scanf("%d",&step);
@@ -24,11 +28,11 @@ int main(){
                break;
        case 2: Step_2();
                break;
-       case 3: Step_3();
+       case 3: Invest(Mymoney);
                break;
      }
-    step=-1;  
-   } 
+    step=-1;
+   }
 
 
 
@@ -36,27 +40,34 @@ int main(){
 }
 void Step_1()
 {
-   printf("11111");
-
+   system("clear");
+   printf("This is Stock corporation\n");
 }
 void Step_2()
-{ 
-   printf("22222");
-
+{
+   system("clear");
+   printf("22222\n");
 }
 void Step_3()
-{ 
-   printf("33333");
-
+{
+  system("clear");
+  printf("33333\n");
 }
-
+void Invest(int money)
+{
+  printf("1.Press the Initial investment\n");
+  scanf("%d",&Mymoney);
+  printf("2.Press the Corporation Number\n");
+  scanf("%d",&Cornum);
+  printf("Investment fund is %d, Corporation is %s",Mymoney,Corporation[15][Cornum]);
+}
 void DisplayMenu(){
    printf("**********************************************************\n");
    printf("******This is Stock Simulation of Embeddedsw Project******\n");
    printf("**********************************************************\n");
    printf("1. Comfirm the corporation\n");
    printf("2. Check the price-earnings ratio of corporation\n");
-   printf("3. Invest!!! \n");\
+   printf("3. Invest!!! \n");
    printf("**********************************************************\n");
    printf("**********************************************************\n");
    printf("**********************************************************\n");
