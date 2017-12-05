2fc0d22003daaddb595336de1db0183f0976b268
jungjunkim <jungjun9150@naver.com>
2017-12-05 06:59:03 -0800

    calculate header make

---
+++ calcurate.h
@@ -0,0 +1,17 @@
+#ifndef
+#include CALCURATE_H
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+long long calcurate(int money)
+{
+  long long ret;
+
+  return ret;
+ 
+}
