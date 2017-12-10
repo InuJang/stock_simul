f6b56fbe5ced89a16755ad2cb2109c551bf1b0a9
jungjunkim <jungjun9150@naver.com>
2017-12-10 09:52:15 -0800

    shared library of jfuncs // with func1 func2

---
+++ stock_data/jfuncs.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+void write_parsing(char *stock_name);
+void change(char *stock_name);
+
+void main()
+{
+	int fd3,fd4;
+	int SZ_BUF = 1024;
+	char textbuf[4096];
+	char *line;
+	char ch;
+	char txt[4] ={'.','t','x','t'} ;
+	char stock_change[20];
+	int cnt = 0;
+	fd3= fopen("list.txt","r");
+	while(fgets(textbuf,sizeof(textbuf),fd3)!=NULL)
+	{
+		while(1)
+		{
+			if(*(textbuf+cnt)=='\n')
+			{
+				*(textbuf+cnt) = 0;
+				break;
+			}
+			cnt++;
+		}
+		strcat(textbuf,txt);
+		change(textbuf);
+		cnt = 0;
+	}
+	close(fd3);
+
+
+}
